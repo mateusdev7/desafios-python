bcdc0eb9b300ff08b70fd826151dc41078757ae5
unknown <mateusdev1337@gmail.com>
2021-10-04 09:53:14 -0300

    Adicionando emoji na linha 12

--- pythonDesafios/desafio046.py
+++ pythonDesafios/desafio046.py
@@ -1,5 +1,7 @@
 from time import sleep
 
+#U+1F973 (Emoji de comemoração)
+
 print('-=-' * 7)
 print('\033[1;31mContagem regressiva\033[m')
 print('-=-' * 7)
@@ -7,4 +9,4 @@ print('-=-' * 7)
 for cont in range(10, -1, -1):
     sleep(0.5)
     print(cont)
-print('\033[1;32mEXPLOSÃO DE FOGOS DE ARFICIOS\033[m')
+print('\033[1;32mEXPLOSÃO DE FOGOS DE ARFICIOS\033[m \U0001F973')
