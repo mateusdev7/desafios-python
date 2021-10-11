8081e46914063d978759ca1664ca4fc5a98db527
MateusDev7 <mateusdev1337@gmail.com>
2021-10-11 09:37:29 -0300

    Alteração na seta e na linha 6
    Mudei o formato da setinha e deixei ao "Acabou" em maiúsculas.

--- pythonDesafios/desafio051.py
+++ pythonDesafios/desafio051.py
@@ -2,5 +2,5 @@ primeiro_termo = int(input('Digite o primeiro termo de uma P.A: '))
 razao = int(input('Digite a razão dessa P.A: '))
 decimo = primeiro_termo + 10 * razao
 for c in range(primeiro_termo, decimo, razao):
-    print(f'{c}', end=' -> ')
-print('Acabou')
+    print(f'{c}', end=' → ')
+print('ACABOU')
