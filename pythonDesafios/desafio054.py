f963bf30362b20c96a27fe89a80e62d03e934159
MateusDev7 <mateusdev1337@gmail.com>
2021-10-13 11:07:19 -0300

    Adicionando Comentários
    Adicionei alguns comentários para entender melhor o código posteriormente.

--- pythonDesafios/desafio054.py
+++ pythonDesafios/desafio054.py
@@ -1,11 +1,11 @@
-from datetime import date
+from datetime import date # Importando a biblioteca da data atual
 
-ano_atual = date.today().year
-totalmaior = 0
-totalmenor = 0
-for c in range(1,8):
+ano_atual = date.today().year # Pegando o ano atual
+totalmaior = 0 # Soma das pessoas maiores de idade
+totalmenor = 0 # Soma das pessoas menores de idade
+for c in range(1,8): # Perguntando a idade de 7 pessoas
     nascimento = int(input(f'Em qual ano a {c}º pessoa nasceu? '))
-    idade = ano_atual - nascimento
+    idade = ano_atual - nascimento # Idade atual da pessoa
     if idade >= 21:
         totalmaior += 1
     else:
