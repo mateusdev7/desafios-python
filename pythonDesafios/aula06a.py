3e4d11b186d65c7a3b601060916ba1ab8a8ddd07
MateusDev7 <mateusdev1337@gmail.com>
2021-10-20 11:32:08 -0300

    Adicionando 2 linhas para testar o git

--- pythonDesafios/aula06a.py
+++ pythonDesafios/aula06a.py
@@ -1,4 +1,7 @@
 n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
 s = n1+n2
+media = (n1+n2) / 2
 print('A soma entre {} e {} vale {}'.format(n1,n2,s))
+print('A média entre {} e {} vale {}'.format(n1, n2, media))
+
