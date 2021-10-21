ada43ee01a4b3c28aba36d1226d76b2963a94b1f
MateusDev7 <mateusdev1337@gmail.com>
2021-10-21 12:16:46 -0300

    Mudando o nome da variável

--- pythonDesafios/desafio018.py
+++ pythonDesafios/desafio018.py
@@ -2,7 +2,7 @@ from math import radians, sin, cos, tan
 
 angulo = int(input('Digite o valor de um ângulo: '))
 sen = sin(radians(angulo))  # Pegando o valor em radianos e convertendo para seno
-cos = cos(radians(angulo))  # Pegando o valor em radianos e convertendo para cosseno
+coss = cos(radians(angulo))  # Pegando o valor em radianos e convertendo para cosseno
 tg = tan(radians(angulo))  # Pegando o valor em radianos e convertendo para tangente
 print(f'O seno desse ângulo é {sen:.2f}\nO cosseno desse ângulo é {cos:.2f}\nA tangente desse ângulo é {tg:.2f}')
 
