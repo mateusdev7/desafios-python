9955c103bb34bdfad886bcc6255b509d0b06a089
MateusDev7 <mateusdev1337@gmail.com>
2021-11-04 10:53:28 -0300

    Simulador de caixa eletrônico

---
+++ pythonDesafios/desafio071.py
@@ -0,0 +1,21 @@
+valor = int(input('Qual o valor que você deseja sacar? R$'))
+total = valor
+cedula = 50
+totcedulas = 0
+while True:
+    if total >= cedula:
+        total -= cedula
+        totcedulas += 1
+    else:
+        if totcedulas > 0:
+            print(f'Total de {totcedulas} cédulas de R${cedula:.2f}')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 1
+        totcedulas = 0
+        if total == 0:
+            break
+
