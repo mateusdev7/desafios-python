34027ec8a77024f2f3afb4fd91b4f9ec24b14af3
MateusDev7 <mateusdev1337@gmail.com>
2021-11-06 14:54:29 -0300

    Aulas sobre Tuplas

--- pythonDesafios/aula16.py
+++ pythonDesafios/aula16.py
@@ -1,16 +1,16 @@
 lanche = ('Hambúrguer','Suco','Pizza','Pudim','Batata Frita')
 
-# for comida in lanche:
-#     print(f'Eu vou comer {comida}')
-# print('HMMM, comi demais!!!')
+for comida in lanche:
+    print(f'Eu vou comer {comida}')
+print('HMMM, comi demais!!!')
 
-# for cont in range(0, len(lanche)):
-#     print(f'Eu vou comer {lanche[cont]}')
-# print('HMMM, comi demais!!!')
+for cont in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[cont]}')
+print('HMMM, comi demais!!!')
 
-# for pos, comida in enumerate(lanche):
-#      print(f'Eu vou comer {comida} na posicao {pos}')
-# print('HMMM, comi demais!!!')
+for pos, comida in enumerate(lanche):
+     print(f'Eu vou comer {comida} na posicao {pos}')
+print('HMMM, comi demais!!!')
 
 print(lanche[0])
 print(lanche[:2])
---
+++ pythonDesafios/desafio072.py
@@ -0,0 +1,2 @@
+numeros = ('Um','Dois','Trê','Quatro','Cinco','Seis','Sete','Oito','Nove','Dez','Onze','Doze','Treze',
+'Quartoze','Quinze','Dezesseis','Dezesete','Dezoito','Dezenove','Vinte')
