2505643e9514e40a04a51646d726cd70baf834b3
MateusDev7 <mateusdev1337@gmail.com>
2021-11-06 15:15:35 -0300

    Lendo um número por extenso

--- pythonDesafios/desafio072.py
+++ pythonDesafios/desafio072.py
@@ -1,2 +1,9 @@
-numeros = ('Um','Dois','Trê','Quatro','Cinco','Seis','Sete','Oito','Nove','Dez','Onze','Doze','Treze',
-'Quartoze','Quinze','Dezesseis','Dezesete','Dezoito','Dezenove','Vinte')
+numeros_tupla = ('Zero','Um','Dois','Trê','Quatro','Cinco','Seis','Sete','Oito','Nove','Dez','Onze','Doze','Treze',
+'Quartoze','Quinze','Dezesseis','Dezessete','Dezoito','Dezenove','Vinte')
+
+while True:
+    usuario_numero = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= usuario_numero <= 20:
+        break
+    print('Tente novamente. ', end='')
+print(f'Você digitou o número {numeros_tupla[usuario_numero]}')
