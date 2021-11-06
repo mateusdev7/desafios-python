ec94619130230597a9205357317652889242722d
MateusDev7 <mateusdev1337@gmail.com>
2021-11-06 14:34:41 -0300

    Aulas sobre Tuplas

---
+++ pythonDesafios/aula16.py
@@ -0,0 +1,23 @@
+lanche = ('Hambúrguer','Suco','Pizza','Pudim','Batata Frita')
+
+# for comida in lanche:
+#     print(f'Eu vou comer {comida}')
+# print('HMMM, comi demais!!!')
+
+# for cont in range(0, len(lanche)):
+#     print(f'Eu vou comer {lanche[cont]}')
+# print('HMMM, comi demais!!!')
+
+# for pos, comida in enumerate(lanche):
+#      print(f'Eu vou comer {comida} na posicao {pos}')
+# print('HMMM, comi demais!!!')
+
+print(lanche[0])
+print(lanche[:2])
+print(lanche[0:2])
+print(lanche[1:])
+print(lanche[-1])
+print(len(lanche))
+print(sorted(lanche))
+for c in lanche:
+    print(c)
---
+++ pythonDesafios/aula16b.py
@@ -0,0 +1,9 @@
+a = (1,2,3)
+b = (4,5,6)
+c = a + b
+d = b + a
+print(c)
+print(d)
+print(f'O número 5 aparece {c.count(5)} vez')
+print(f'O número 6 está na posição {c.index(6)}')
+
