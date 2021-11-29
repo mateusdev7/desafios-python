04abbf3c5c3e4af203108b69599cedd840997bf5
mateusdev7 <mateusdev1337@gmail.com>
2021-11-29 15:43:04 -0300

    Sorteio de números
    Maior e Menor valor

---
+++ pythonDesafios/desafio074.py
@@ -0,0 +1,8 @@
+import random
+maior = menor = 0
+
+sorteio = tuple(random.sample(range(10),5))
+print('Os valores sorteados são: ', end='')
+for n in sorteio:
+    print(f'{n} ', end='')
+print(f'O maior valor sorteado foi {max(sorteio)} o menor valor sorteado foi {min(sorteio)}')
