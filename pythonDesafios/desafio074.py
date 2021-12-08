623fed781b5fdfa9bad89d5e239272a0be0bfc8f
mateusdev7 <mateusdev1337@gmail.com>
2021-12-08 10:23:20 -0300

    Sorteando Números na Tupla
    Adicionei um sinal para identificar melhor os números sorteados

--- pythonDesafios/desafio074.py
+++ pythonDesafios/desafio074.py
@@ -4,5 +4,5 @@ maior = menor = 0
 sorteio = tuple(random.sample(range(10),5))
 print('Os valores sorteados são: ', end='')
 for n in sorteio:
-    print(f'{n} ', end='')
-print(f'O maior valor sorteado foi {max(sorteio)} o menor valor sorteado foi {min(sorteio)}')
+    print(f'{n} ', end='-> ')
+print(f'\nO maior valor sorteado foi {max(sorteio)} o menor valor sorteado foi {min(sorteio)}')
