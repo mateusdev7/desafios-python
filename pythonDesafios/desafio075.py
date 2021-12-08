8c6d6595577089889a2b5065cd6be35484589425
mateusdev7 <mateusdev1337@gmail.com>
2021-12-08 14:07:50 -0300

    Analisando valores em Tupla

--- pythonDesafios/desafio075.py
+++ pythonDesafios/desafio075.py
@@ -1,11 +1,12 @@
 pares = 0
 numeros = tuple(int(input(f'Digite um número:\n'))for c in range(1,5))
 
-numero_nove = numeros.count(9)
-numero_tres = numeros.index(3)
-print(f'O número 9 apareceu {numero_nove} vez(es)')
-print(f'O número 3 foi digitado na {numero_tres + 1}º posição')
+print(f'\nO número 9 apareceu {numeros.count(9)} vez(es)')
+if 3 in numeros:
+    print(f'O número 3 foi digitado na {numeros.index(3) + 1}º posição')
+else:
+    print('O número 3 não foi digitado em nenhuma posição')
 for n in numeros:
     if n % 2 == 0:
-        pares += 1
-print(f'Foram digitados {pares} números pares')
+        print(f'Foram digitados {n} números pares')
+        break
