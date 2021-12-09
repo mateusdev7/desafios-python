02ce2be371f1c394219eadd10612174b5a8f577a
mateusdev7 <mateusdev1337@gmail.com>
2021-12-09 18:31:19 -0300

    Adicionando valores a uma lista
    Alterei algumas informações para ficarem melhores de serem lidas.

--- pythonDesafios/desafio079.py
+++ pythonDesafios/desafio079.py
@@ -4,10 +4,11 @@ nome = str(input('Qual é o seu nome? '))
 print(f'Olá, {nome} vamos começar...\n')
 while True:
     valor = int(input('Digite um valor: '))
-    if valor in lista:
-        print('Esse valor já está na lista, não podemos adicionar valores repetidos')
     if valor not in lista:
+        print('Valor adicionado a lista')
         lista.append(valor)
+    else:
+        print('Esse valor já está na lista, não podemos adicionar valores repetidos')
     opcao = str(input('Quer inserir outro valor [S/N]? ').upper().strip()[0])
     while opcao not in 'SN':
         opcao = str(input('Quer inserir outro valor [S/N]?').upper().strip()[0])
@@ -15,4 +16,4 @@ while True:
         print("\n========== PROGRAMA ENCERRADO ==========")
         break
 lista.sort()
-print(f'Olá {nome} você inseriu os valores {lista}')
+print(f'Opa {nome}, você inseriu {len(lista)} números a lista, que são -> {lista}')
